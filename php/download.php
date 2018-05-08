f19282bf9d13aa420a46bf2e2a431e5fede4b1b6
nikolay <kvasov1992@inbox.ru>
2018-05-08 17:51:23 +0400

    Не готовый скрипт, четвёртый коммит ветки Works

---
+++ php/download.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once("connect.php");
+require_once("functions.php");
+
+$show_data = get_data();
+
+print_r($show_data);
+
+
+?>
--- php/handler.php
+++ php/handler.php
@@ -1,10 +1,9 @@
 <?php
-mb_internal_encoding("UTF-8");
 
 include_once("connect.php");
 
 if (isset($_POST['submit'])) {
-	$client_surname = strip_tags(trim ($_POST['element_1']));
+	$client_surname = strip_tags(trim ($_POST['element_1'])); // Проверка полей формы
 	$client_name = strip_tags(trim ($_POST['element_2']));
 	$client_second_name = strip_tags(trim ($_POST['element_3']));
 	$client_sex = strip_tags(trim ($_POST['element_6']));
@@ -13,14 +12,14 @@ if (isset($_POST['submit'])) {
 	$client_year_birthday = strip_tags(trim ($_POST['element_4_3']));
 	$client_weight = strip_tags(trim ($_POST['element_12']));
 	$client_height = strip_tags(trim ($_POST['element_14']));
-
+                                                                    //Заполнение таблицы в БД значениями из полей формы 
 	$result = mysql_query (" 
+                                                            
+		INSERT INTO input_data(surname, name, second_name, sex, day_birthday, month_birthday, year_birthday, weight, height) 
+		VALUES ('$client_surname', '$client_name', '$client_second_name', '$client_sex','$client_day_birthday','$client_month_birthday', '$client_year_birthday', '$client_weight', '$client_height')
 
-		INSERT INTO input_data(surname, name, second_name, sex, day_birthday, month_birthday, year_birthday, weight, height, recording_time) 
-		VALUES ('$client_surname', '$client_name', '$client_second_name', '$client_sex','$client_day_birthday','$client_month_birthday', '$client_year_birthday', '$client_weight', '$client_height', '".time()."' )
+	                      ");
 
-	");
-	
 	mysql_close();
 } 
 header('Location: http://daltasert.ru/download.html');
